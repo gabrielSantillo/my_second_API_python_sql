7db5677a05218f2f943d7d069ae1b87001fe7af2
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-10-05 16:08:28 -0600

    Finished the knowledge check

--- app.py
+++ app.py
@@ -1,48 +1,132 @@
+# importing librearies to use when making the db connection and building the API
 from flask import Flask, request
 from dbhelpers import run_statement
 import json
 
+# calling the Flask function which will return a value that I will be used for my API
 app = Flask(__name__)
 
+# making a get request with the /api/item endpoint
 @app.get('/api/item')
+# function that will call the procedure responsible to send back all items
 def get_all_items():
+    # calling the procedure
     results = run_statement("CALL get_all_items()")
+    # checking to see if the response is a list and if yes, turn this response into a JSON, if not, sent back a message
     if(type(results) == list):
         items_json = json.dumps(results, default=str)
         return items_json
     else:
         return "Sorry, something has gone wrong."
 
+# making a get request with the /api/item endpoint
 @app.post('/api/item')
+# function that will call the procedure responsible to insert an item
 def insert_item():
+    # grabbing all data needed to insert an item
     item_name = request.json.get('item_name')
     item_descprition = request.json.get('item_descprition')
     item_stock = request.json.get('item_stock')
+    # calling the procedure responsible to insert an item
     results = run_statement("CALL insert_item(?,?,?)", [item_name, item_descprition, item_stock])
+    # checking to see if the response is a list and if yes, turn this response into a JSON, if not, sent back a message
     if(type(results) == list):
         id_item_json = json.dumps(results, default=str)
         return id_item_json
     else:
         return "Sorry, something has gone wrong."
 
+# making a patch request with the /api/item endpoint
 @app.patch('/api/item')
+# function that will update the stock of an item
 def update_item_stock():
+    # grabbing all data needed to update the stock item
     item_id = request.json.get('item_id')
     item_stock = request.json.get('item_stock')
+    # calling the procedure responsible to update the stock
     results = run_statement("CALL update_stock_by_item_id(?,?)", [item_id, item_stock])
+    # checking to see if the response is a list and if yes, turn this response into a JSON, if not, sent back a message
     if(type(results) == list):
         id_stock_json = json.dumps(results, default=str)
         return id_stock_json
     else:
         return "Sorry, something has gone wrong."
 
+# making a delete request with the /api/item endpoint
 @app.delete('/api/item')
+# function that will delete an item
 def delete_item():
+    # grabbing the data needed to delete an item
     item_id = request.json.get('item_id')
+    # calling the procedure that will delete an item
     results = run_statement("CALL delete_item_by_id(?)", [item_id])
+    # checking to see if the response is a list and if yes, turn this response into a JSON, if not, sent back a message
     if(type(results) != list):
         return "Item deleted."
     else:
         return "Sorry, something has gone wrong."
 
+# making a get request with the /api/employee endpoint
+@app.get('/api/employee')
+# function that will get an employee based on its id
+def get_employee_by_id():
+    # grabbing the data needed to get the employee
+    employee_id = request.args.get('employee_id')
+    # calling the prcedure responsible to get the employee
+    results = run_statement("CALL get_employee_by_id(?)", [employee_id])
+    # checking to see if the response is a list and if yes, turn this response into a JSON, if not, sent back a message
+    if(type(results) == list):
+        employee_json = json.dumps(results, default=str)
+        return employee_json
+    else:
+        return "Sorry, something has gone wrong."
+
+# making a post request with the /api/employee endpoint
+@app.post('/api/employee')
+# function that will insert an employee
+def insert_employee():
+    # grabbing all data needed to insert an employee
+    employee_name = request.json.get('employee_name')
+    employee_position = request.json.get('employee_position')
+    employee_hourly_wage = request.json.get('employee_hourly_wage')
+    # calling the procedure that will insert an employee
+    results = run_statement("CALL insert_employee(?,?,?)", [employee_name, employee_position, employee_hourly_wage])
+    # checking to see if the response is a list and if yes, turn this response into a JSON, if not, sent back a message
+    if(type(results) == list):
+        employee_json = json.dumps(results, default=str)
+        return employee_json
+    else:
+        return "Sorry, something has gone wrong."
+
+# making a patch request with the /api/employee endpoint
+@app.patch('/api/employee')
+# function that will update the hourly wage based on an employee id
+def update_hourly_wage():
+    # grabbing all data needed to update the hourly wage
+    employee_id = request.json.get('employee_id')
+    employee_hourly_wage = request.json.get('employee_hourly_wage')
+    # calling the procedure that will update the hourly wage
+    results = run_statement("CALL update_hourly_wage(?,?)", [employee_id, employee_hourly_wage])
+    # checking to see if the response is a list and if yes, turn this response into a JSON, if not, sent back a message
+    if(type(results) == list):
+        employee_hourly_wage_json = json.dumps(results, default=str)
+        return employee_hourly_wage_json
+    else:
+        return "Sorry, something has gone wrong."
+
+# making a delete request with the /api/employee endpoint
+@app.delete('/api/employee')
+# caling the function that will delete an employee based on its id
+def delete_employee():
+    # grabbing the data needed to delete an employee
+    employee_id = request.json.get('employee_id')
+    # calling the procedure that will delete an employee
+    results = run_statement("CALL delete_employee_by_id(?)", [employee_id])
+    # checking to see if the response is a list and if yes, turn this response into a JSON, if not, sent back a message
+    if(type(results) != list):
+        return "Employee deleted."
+    else:
+        return "Sorry, something has gone wrong."
+
+# starting our application flask server with debug mode turned on
 app.run(debug=True)
---
+++ my_second_api.sql
@@ -0,0 +1,274 @@
+-- MariaDB dump 10.19  Distrib 10.6.9-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: my_second_api_python_sql
+-- ------------------------------------------------------
+-- Server version	10.6.9-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `postion` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `hired_at` datetime DEFAULT NULL,
+  `hourly_wage` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'gabriel','director','2022-10-05 00:00:00',50),(2,'natalia','manager','2022-10-05 00:00:00',35),(3,'daniel','assistant','2022-10-05 00:00:00',20),(4,'jose','cfo','2022-10-05 00:00:00',400),(7,'first insert','position test','2022-10-05 15:45:36',10);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `description` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `in_stock_quantity` int(10) unsigned DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'corolla','item one',65,'2022-10-04'),(2,'civic','item two',1000000,'2022-10-04'),(6,'jetta','item six',12,'2022-10-04'),(7,'tiguan','item seven',45,'2022-10-04'),(8,'mustang','item eight',545,'2022-10-04'),(9,'camaro','item nine',43,'2022-10-04'),(10,'corvette','item ten',2,'2022-10-04'),(12,'x1','testing car',5,'2022-10-04'),(13,'x1','testing car',5,'2022-10-04'),(14,'testing','testing',45,'2022-10-04'),(15,'first post','first post',42,'2022-10-04');
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'my_second_api_python_sql'
+--
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `delete_employee_by_id` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_employee_by_id`(id_input int unsigned)
+    MODIFIES SQL DATA
+begin
+	delete from employee
+	where employee.id = id_input;
+	commit;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `delete_item_by_id` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_item_by_id`(id_input int unsigned)
+    MODIFIES SQL DATA
+begin
+	delete from item
+	where item.id = id_input;
+	commit;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `get_all_items` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_all_items`()
+begin
+	select *
+	from item;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `get_employee_by_id` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_employee_by_id`(id_input int unsigned)
+begin
+	select e.id, e.name, e.postion, e.hired_at, e.hourly_wage 
+	from employee e
+	where e.id = id_input;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `insert_employee` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_employee`(name_input varchar(100), position_input varchar(100), hourly_wage_input int unsigned)
+    MODIFIES SQL DATA
+begin
+	insert into employee (name, postion, hired_at, hourly_wage)
+	values (name_input, position_input, now(), hourly_wage_input);
+	select e.id 
+	from employee e
+	order by e.id desc
+	limit 1;
+	commit;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `insert_item` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_item`(name_input varchar(100), description_input varchar(100), quantity_in_stock_input int unsigned)
+    MODIFIES SQL DATA
+begin
+	insert into item (name, description, in_stock_quantity, created_at)
+	values (name_input, description_input, quantity_in_stock_input, '2022-10-04');
+	select i.id 
+	from item i
+	order by i.id desc 
+	limit 1;
+	commit;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `update_hourly_wage` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_hourly_wage`(id_input int unsigned, hourly_wage_input int unsigned)
+    MODIFIES SQL DATA
+begin
+	update employee e 
+	set e.hourly_wage = hourly_wage_input
+	where e.id = id_input;
+	select e.hourly_wage 
+	from employee e 
+	where e.id = id_input;
+	commit;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `update_stock_by_item_id` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_stock_by_item_id`(id_input int unsigned, in_stock_input int unsigned)
+    MODIFIES SQL DATA
+begin
+	update item i
+	set i.in_stock_quantity = in_stock_input
+	where i.id = id_input;
+	select i.in_stock_quantity 
+	from item i 
+	where i.id = id_input;
+	commit;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-05 15:52:27
